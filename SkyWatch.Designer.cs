5ccd01e6bce8ea503850725495a9815bbd55e496
adnel <adnelil@bfk.no>
2025-03-26 17:06:59 +0100

    blah blu

--- SkyWatch.Designer.cs
+++ SkyWatch.Designer.cs
@@ -29,26 +29,102 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SkyWatch));
-            textBox1 = new TextBox();
+            ByHenter = new TextBox();
+            HentVær = new Button();
+            Langtidsvarsel = new Button();
+            IdagVær = new Button();
+            Temp = new Label();
+            Vær = new Label();
+            VindHas = new Label();
             SuspendLayout();
             // 
-            // textBox1
+            // ByHenter
             // 
-            textBox1.Location = new Point(338, 12);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(152, 27);
-            textBox1.TabIndex = 0;
-            textBox1.Text = "Skriv inn din by her:";
+            ByHenter.Location = new Point(296, 9);
+            ByHenter.Margin = new Padding(3, 2, 3, 2);
+            ByHenter.Name = "ByHenter";
+            ByHenter.Size = new Size(134, 23);
+            ByHenter.TabIndex = 0;
+            ByHenter.Text = "Skriv inn din by her:";
+            // 
+            // HentVær
+            // 
+            HentVær.Location = new Point(453, 8);
+            HentVær.Margin = new Padding(3, 2, 3, 2);
+            HentVær.Name = "HentVær";
+            HentVær.Size = new Size(82, 22);
+            HentVær.TabIndex = 1;
+            HentVær.Text = "Hent vær";
+            HentVær.UseVisualStyleBackColor = true;
+            HentVær.Click += HentVær_Click;
+            // 
+            // Langtidsvarsel
+            // 
+            Langtidsvarsel.Location = new Point(673, 350);
+            Langtidsvarsel.Name = "Langtidsvarsel";
+            Langtidsvarsel.Size = new Size(95, 23);
+            Langtidsvarsel.TabIndex = 2;
+            Langtidsvarsel.Text = "Langtidsvarsel";
+            Langtidsvarsel.UseVisualStyleBackColor = true;
+            // 
+            // IdagVær
+            // 
+            IdagVær.Location = new Point(673, 299);
+            IdagVær.Name = "IdagVær";
+            IdagVær.Size = new Size(95, 23);
+            IdagVær.TabIndex = 3;
+            IdagVær.Text = "Været i dag";
+            IdagVær.UseVisualStyleBackColor = true;
+            // 
+            // Temp
+            // 
+            Temp.AutoSize = true;
+            Temp.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Temp.ForeColor = SystemColors.ButtonHighlight;
+            Temp.Location = new Point(296, 60);
+            Temp.Name = "Temp";
+            Temp.Size = new Size(38, 21);
+            Temp.TabIndex = 4;
+            Temp.Text = "erer";
+            // 
+            // Vær
+            // 
+            Vær.AutoSize = true;
+            Vær.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Vær.ForeColor = SystemColors.ButtonHighlight;
+            Vær.Location = new Point(296, 81);
+            Vær.Name = "Vær";
+            Vær.Size = new Size(38, 21);
+            Vær.TabIndex = 5;
+            Vær.Text = "erer";
+            // 
+            // VindHas
+            // 
+            VindHas.AutoSize = true;
+            VindHas.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            VindHas.ForeColor = SystemColors.ButtonHighlight;
+            VindHas.Location = new Point(296, 102);
+            VindHas.Name = "VindHas";
+            VindHas.Size = new Size(38, 21);
+            VindHas.TabIndex = 6;
+            VindHas.Text = "erer";
             // 
             // SkyWatch
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DimGray;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(892, 513);
-            Controls.Add(textBox1);
+            ClientSize = new Size(780, 385);
+            Controls.Add(VindHas);
+            Controls.Add(Vær);
+            Controls.Add(Temp);
+            Controls.Add(IdagVær);
+            Controls.Add(Langtidsvarsel);
+            Controls.Add(HentVær);
+            Controls.Add(ByHenter);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "SkyWatch";
             Text = "SkyWatch";
             Load += SkyWatch_Load;
@@ -58,6 +134,12 @@
 
         #endregion
 
-        private TextBox textBox1;
+        private TextBox ByHenter;
+        private Button HentVær;
+        private Button Langtidsvarsel;
+        private Button IdagVær;
+        private Label Temp;
+        private Label Vær;
+        private Label VindHas;
     }
 }
--- SkyWatch.cs
+++ SkyWatch.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Windows.Forms;
 using DotNetEnv;
 
@@ -7,7 +11,6 @@ namespace VærApp
     public partial class SkyWatch : Form
     {
         string apiKey;
-
         public SkyWatch()
         {
             InitializeComponent();
@@ -17,7 +20,69 @@ namespace VærApp
 
         private void SkyWatch_Load(object sender, EventArgs e)
         {
-            
+        }
+
+        private async void HentVær_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?q={ByHenter.Text}&appid={apiKey}&units=metric";
+                Debug.WriteLine(url);
+                using (HttpClient client = new HttpClient())
+                {
+                    string json = await client.GetStringAsync(url);
+                    Debug.WriteLine(json);
+                    var weatherData = JsonSerializer.Deserialize<WeatherResponse>(json);
+
+                    if (weatherData?.Main != null && weatherData.Weather != null && weatherData.Weather.Count > 0)
+                    {
+                        Temp.Text = $"Temperatur: {weatherData.Main.Temp}°C";
+                        Vær.Text = $"Vær: {weatherData.Weather[0].description}";
+                        VindHas.Text = $"Vindhastighet: {weatherData.Wind.speed} m/s";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Weather data could not be retrieved.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
+
+    public class WeatherResponse
+    {
+        [JsonPropertyName("main")]
+        public MainInfo Main { get; set; }
+
+        [JsonPropertyName("weather")]
+        public List<WeatherCondition> Weather { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Wind Wind { get; set; }
+    }
+
+    public class MainInfo
+    {
+        [JsonPropertyName("temp")]
+        public float Temp { get; set; }
+    }
+
+    public class WeatherCondition
+    {
+        [JsonPropertyName("main")]
+        public string description { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonPropertyName("speed")]
+        public float speed { get; set; }
+
+    }
 }
+        
+    
